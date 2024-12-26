9216a92b085b8edefd15d8a4727a589515df22b7
ayten-github <ababakishiyeva@std.beu.edu.az>
2024-12-26 13:05:15 +0400

    initial commit

---
+++ src/main/java/az/edu/turing/bookingappspring/BookingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package az.edu.turing.bookingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/edu/turing/bookingappspring/BookingAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing.bookingappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
